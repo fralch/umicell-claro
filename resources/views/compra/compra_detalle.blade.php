c6b0921f0b847d55b8af02cf85b01a7674d4e2b5
Fralch <ingfralch@gmail.com>
2020-08-01 17:43:30 -0500

    sin fuentes

--- resources/views/compra/compra_detalle.blade.php
+++ resources/views/compra/compra_detalle.blade.php
@@ -13,7 +13,7 @@
     <link rel="shortcut icon" type="image/png" href="/images/icono.png" />
 ​
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 ​
     <!-- Custom styles for this template-->
